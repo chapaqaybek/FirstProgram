c908038e7b53ee6b9314538ea2645d9f2720a0e0
chapaqaybek <s.numonov@newuu.uz>
2025-01-16 10:08:52 +0500

    Hello message and age

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << "Hello, Qo'zivoy" << endl;
+    cout << "How old are you << endl;
+    cout << "Anig'ini ayt:" << endl;
+    cin >>age;
+    cout << "Yoshing" << age << endl;
+    return 0;
+}
+//endl means endline
